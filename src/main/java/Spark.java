d55651de3ac4bb1575a1e5dbe28b59be1ef8a8e2
TehDrink <kesterteh@gmail.com>
2026-01-23 02:00:07 +0800

    Renamed Duke to Spark, added Greet and exit

---
+++ src/main/java/Spark.java
@@ -0,0 +1,20 @@
+public class Spark {
+    public static void main(String[] args) {
+
+//        Level-0
+//        Rename, Greet, Exit
+        String logo = " ____                   _    \n" +
+                "/ ___| _ __   __ _ _ __| | __\n" +
+                "\\___ \\| '_ \\ / _` | '__| |/ /\n" +
+                " ___) | |_) | (_| | |  |   <\n" +
+                "|____/| .__/ \\__,_|_|  |_|\\_\\ \n" +
+                "      |_|";
+        System.out.println(logo);
+        System.out.println("----------------");
+        System.out.println("Bark! Hello I am Spark! I am dog!");
+        System.out.println("What can I fetch for you today, Bark?");
+        System.out.println("----------------");
+        System.out.println("See you soon! Bark!\n");
+
+    }
+}
