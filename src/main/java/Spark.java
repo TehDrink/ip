7f7818c182e0c288e21dc70a6c90032092b17aae
TehDrink <kesterteh@gmail.com>
2026-01-23 14:34:53 +0800

    Added Echo

--- src/main/java/Spark.java
+++ src/main/java/Spark.java
@@ -1,8 +1,11 @@
-public class Spark {
-    public static void main(String[] args) {
+import java.util.Objects;
+import java.util.Scanner;  // Import the Scanner class
 
-//        Level-0
-//        Rename, Greet, Exit
+public class Spark {
+
+    // Level-0
+    // Rename, Greet, Exit
+    public static void greet() {
         String logo = " ____                   _    \n" +
                 "/ ___| _ __   __ _ _ __| | __\n" +
                 "\\___ \\| '_ \\ / _` | '__| |/ /\n" +
@@ -13,6 +16,32 @@ public class Spark {
         System.out.println("----------------");
         System.out.println("Bark! Hello I am Spark! I am dog!");
         System.out.println("What can I fetch for you today, Bark?");
+    }
+
+    // Level-1
+    // Echo
+    public static boolean echo() {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+
+        if (text.equals("bye")) {
+            return false;
+        } else {
+            System.out.println("----------------");
+            System.out.println(text);
+            System.out.println("----------------");
+
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Greet the user
+        greet();
+        // Initiate Echo
+        while (echo()) {
+            // While echo is true
+        }
         System.out.println("----------------");
         System.out.println("See you soon! Bark!\n");
 
